89c02728423ba5c6b752d4a37107064b17fc04eb
Eric Chan <eric.chan@accelbyte.net>
2024-03-03 01:50:01 -0800

    fix: correct back springboot to the previous packages + add missing mocked config for build test

--- src/test/java/net/accelbyte/cloudsave/validator/AppTest.java
+++ src/test/java/net/accelbyte/cloudsave/validator/AppTest.java
@@ -1,10 +1,13 @@
 package net.accelbyte.cloudsave.validator;
 
+import net.accelbyte.cloudsave.validator.config.MockedAppConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(
+		classes = MockedAppConfiguration.class,
+		properties = "spring.main.allow-bean-definition-overriding=true"
+)
 class AppTest {
 
 	@Test
